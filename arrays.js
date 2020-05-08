44101d69a3d50bf2713e085bf78f6efabe0ac2c6
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-08 23:35:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -33,8 +33,7 @@ function destructivelyRemoveElementFromBeginningOfArray(a) {
 }
 
 function removeElementFromBeginningOfArray(a) {
-  a.slice(1)
-  return a;
+  return a.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray(a) {
