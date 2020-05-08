7931612a9b65676aa51effc16f078d6fcac225f4
boisterous-media-9079 <luyao.mly@gmail.com>
2020-05-08 21:47:12 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -4,3 +4,17 @@ var chocolateBars = [
   "kitkat",
   "skittles"
 ]
+
+function addElementToBeginningOfArray(a, b) {
+  var a = [b, ...a]
+  return a;
+}
+
+function destructivelyAddElementToBeginningOfArray(a, b) {
+  return [b, ...a];
+}
+
+function addElementToEndOfArray(a, b) {
+  var a = [...a, b]
+  return a;
+}
